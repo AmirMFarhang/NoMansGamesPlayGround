e8f7c7c2d072cd0fdb7394483d4f632fa795d424
AmirMFarhang <amirmohammadfarhang83@gmail.com>
2023-03-07 11:04:57 +0100

    first cleaning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
+  content: ["./src/**/*.{html,js}", "./dist/**/*.{html,js}"],
   theme: {
+      colors: {
+          transparent: 'transparent',
+          current: 'currentColor',
+          'primary': '#240436',
+          'maintext': '#ffffff',
+
+      },
     container: {
       center: true,
       padding: '2rem'
     },
+    extend: {},
   },
-
-  variants: {},
   plugins: [],
 }
